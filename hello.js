2bad118aef6e106d6f416eaf00f7fb59c85fbb6a
Tyler Julian <tjulian@uber.com>
2018-06-26 22:13:38 -0700

    Add an "add" function

--- hello.js
+++ hello.js
@@ -1,5 +1,9 @@
 // This file does a bunch of hello world things.
 
+// I'm making my own add function
+function add(a, b) {
+  return a - b;
+}
 
 // This is a function that prints hello world.
 function helloWorld() {
